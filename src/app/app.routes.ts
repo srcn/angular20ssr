2005c4a99b1fa28b96c6929d5407207d3363a231
srcn <117369+srcn@users.noreply.github.com>
2025-07-17 19:47:58 +0300

    Add routes and components for first and second routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import SecondRoute from './second-route';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./first-route'),
+  },
+  {
+    path: 'second-route',
+    loadComponent: () => import('./second-route'),
+  },
+];
---
+++ src/app/second-route.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'SecondRoute',
+  imports: [],
+  template: `<h1>Hello from second route</h1>`,
+})
+export default class SecondRoute {}
