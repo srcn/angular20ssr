189139e1ba4d32c72fc6b3d0318b49acd0d7c58a
srcn <117369+srcn@users.noreply.github.com>
2025-07-17 19:53:32 +0300

    Update .gitignore to include .vercel and modify API export to use reqHandler

--- api/index.mjs
+++ api/index.mjs
@@ -1,5 +1,3 @@
-const { app } = await import("../dist/angular20ssr/server/server.mjs");
+const { reqHandler } = await import("../dist/angular20ssr/server/server.mjs");
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-export default app;
+export default reqHandler;
