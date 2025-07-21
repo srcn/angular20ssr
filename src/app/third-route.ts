34f7671c3cd45ab21a41df15bb019661f84586df
srcn <117369+srcn@users.noreply.github.com>
2025-07-21 12:17:30 +0300

    Add third route with Client render mode

---
+++ src/app/third-route.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ThirdRoute',
+  imports: [],
+  template: `<h1>Hello from third route</h1>`,
+})
+export default class ThirdRoute {}
